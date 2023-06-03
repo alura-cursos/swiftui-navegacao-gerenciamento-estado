23516d2bc1d5f4998d3ea7e72827945312de06c6
Giovanna Moeller <giovannagmoeller@gmail.com>
2023-06-03 20:37:53 -0300

    aula 3.4 - apresentando os produtos através de um modal

--- ChefDelivery/StoresView/StoreDetailView.swift
+++ ChefDelivery/StoresView/StoreDetailView.swift
@@ -11,6 +11,7 @@ struct StoreDetailView: View {
     
     let store: StoreType
     @Environment(\.presentationMode) var presentationMode
+    @State private var selectedProduct: ProductType?
     
     var body: some View {
         ScrollView(showsIndicators: false) {
@@ -48,8 +49,8 @@ struct StoreDetailView: View {
                 
                 ForEach(store.products) { product in
                     
-                    NavigationLink {
-                        ProductDetailView(product: product)
+                    Button {
+                        selectedProduct = product
                     } label: {
                         HStack(spacing: 8) {
                             VStack(alignment: .leading, spacing: 8) {
@@ -74,6 +75,9 @@ struct StoreDetailView: View {
                         }
                         .padding()
                         .foregroundColor(.black)
+                    }
+                    .sheet(item: $selectedProduct) { product in
+                        ProductDetailView(product: product)
                     }
                 }
             }
