965529149ea02ef9c25ed9f888fa0b3c337d664d
Giovanna Moeller <giovannagmoeller@gmail.com>
2023-06-03 20:13:28 -0300

    aula 2.1 - construindo o layout da tela de detalhes

--- ChefDelivery/StoresView/StoreDetailView.swift
+++ ChefDelivery/StoresView/StoreDetailView.swift
@@ -12,7 +12,42 @@ struct StoreDetailView: View {
     let store: StoreType
     
     var body: some View {
-        Text(store.name)
+        VStack(alignment: .leading) {
+            Image(store.headerImage)
+                .resizable()
+                .scaledToFit()
+            
+            HStack {
+                Text(store.name)
+                    .font(.title)
+                    .fontWeight(.bold)
+                Spacer()
+                Image(store.logoImage)
+            }
+            .padding(.vertical, 8)
+            .padding(.horizontal)
+            
+            HStack {
+                Text(store.location)
+                
+                Spacer()
+                
+                ForEach(1...store.stars, id: \.self) { _ in
+                    Image(systemName: "star.fill")
+                        .foregroundColor(.yellow)
+                        .font(.caption)
+                }
+            }.padding()
+            
+            Text("Produtos")
+                .font(.title2)
+                .fontWeight(.bold)
+                .padding()
+            
+            Spacer()
+        }
+        .navigationTitle(store.name)
+        .navigationBarTitleDisplayMode(.inline)
     }
 }
 
