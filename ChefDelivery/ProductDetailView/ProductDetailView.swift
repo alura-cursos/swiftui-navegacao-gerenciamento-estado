4af6f96d00c73a2a464f3e1b12c945f187225dd7
Giovanna Moeller <giovannagmoeller@gmail.com>
2023-06-03 20:27:14 -0300

    aula 3.2 - criando layout da tela de produtos

--- ChefDelivery/ProductDetailView/ProductDetailView.swift
+++ ChefDelivery/ProductDetailView/ProductDetailView.swift
@@ -12,7 +12,46 @@ struct ProductDetailView: View {
     let product: ProductType
     
     var body: some View {
-        Text(product.name)
+        VStack {
+            VStack(alignment: .leading, spacing: 16) {
+                Image(product.image)
+                    .resizable()
+                    .scaledToFit()
+                    .shadow(radius: 20)
+                
+                Text(product.name)
+                    .font(.title)
+                    .bold()
+                    .padding(.horizontal)
+                
+                Text(product.description)
+                    .padding(.horizontal)
+                
+                Text(product.formattedPrice)
+                    .font(.title3)
+                    .padding(.horizontal)
+                    .bold()
+            }
+            
+            Spacer()
+            
+            Button {
+                print("Botão pressionado")
+            } label: {
+                HStack {
+                    Image(systemName: "cart")
+                    Text("Adicionar ao carrinho")
+                }
+                .padding(.horizontal, 32)
+                .padding(.vertical, 16)
+                .font(.title3)
+                .bold()
+                .foregroundColor(.white)
+                .background(Color("ColorRed"))
+                .cornerRadius(32)
+                .shadow(color: Color("ColorRedDark").opacity(0.5), radius: 10, x: 6, y: 8)
+            }
+        }
     }
 }
 
