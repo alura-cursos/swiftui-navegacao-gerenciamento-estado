418a19a6aa6a04e12dd64edac28be706ce242f86
Giovanna Moeller <giovannagmoeller@gmail.com>
2023-06-03 19:44:20 -0300

    uploading initial project

---
+++ ChefDelivery/Model/DataSourceMock.swift
@@ -0,0 +1,26 @@
+//
+//  DataSourceMock.swift
+//  ChefDelivery
+//
+//  Created by ALURA on 17/05/23.
+//
+
+import Foundation
+
+let ordersMock: [OrderType] = [
+    OrderType(id: 1, name: "Restaurantes", image: "hamburguer"),
+    OrderType(id: 2, name: "Mercado", image: "mercado"),
+    OrderType(id: 3, name: "Farmácia", image: "farmacia"),
+    OrderType(id: 4, name: "Pet", image: "petshop"),
+    OrderType(id: 5, name: "Descontos", image: "descontos"),
+    OrderType(id: 6, name: "Bebidas", image: "bebidas"),
+    OrderType(id: 7, name: "Gourmet", image: "gourmet"),
+]
+
+let storesMock: [OrderType] = [
+    OrderType(id: 1, name: "Monstro burger", image: "monstro-burger-logo"),
+    OrderType(id: 2, name: "Food court", image: "food-court-logo"),
+    OrderType(id: 3, name: "Carbron", image: "carbron-logo"),
+    OrderType(id: 4, name: "Padaria", image: "bakery-logo"),
+    OrderType(id: 5, name: "Açaí Panda", image: "acai-panda-logo"),
+]
---
+++ ChefDelivery/StoresView/StoresContainerView.swift
@@ -0,0 +1,34 @@
+//
+//  StoresContainerView.swift
+//  ChefDelivery
+//
+//  Created by Ândriu F Coelho on 29/05/23.
+//
+
+import SwiftUI
+
+struct StoresContainerView: View {
+    
+    var title = "Lojas"
+    
+    var body: some View {
+        VStack(alignment: .leading) {
+            Text(title)
+                .font(.headline)
+            
+            VStack(alignment: .leading, spacing: 30) {
+                ForEach(storesMock) { mock in
+                    StoreItemView(order: mock)
+                }
+            }
+        }
+        .padding(.horizontal, 20)
+    }
+}
+
+struct StoresContainerView_Previews: PreviewProvider {
+    static var previews: some View {
+        StoresContainerView()
+            .previewLayout(.sizeThatFits)
+    }
+}
