6f75c17dd39987f1fe35180216118a1924cb9937
Giovanna Moeller <giovannagmoeller@gmail.com>
2023-06-03 20:15:12 -0300

    aula 2.2 - mostrando os produtos na tela de detalhes

--- ChefDelivery/StoresView/StoreDetailView.swift
+++ ChefDelivery/StoresView/StoreDetailView.swift
@@ -12,39 +12,63 @@ struct StoreDetailView: View {
     let store: StoreType
     
     var body: some View {
-        VStack(alignment: .leading) {
-            Image(store.headerImage)
-                .resizable()
-                .scaledToFit()
-            
-            HStack {
-                Text(store.name)
-                    .font(.title)
-                    .fontWeight(.bold)
-                Spacer()
-                Image(store.logoImage)
-            }
-            .padding(.vertical, 8)
-            .padding(.horizontal)
-            
-            HStack {
-                Text(store.location)
+        ScrollView(showsIndicators: false) {
+            VStack(alignment: .leading) {
+                Image(store.headerImage)
+                    .resizable()
+                    .scaledToFit()
                 
-                Spacer()
-                
-                ForEach(1...store.stars, id: \.self) { _ in
-                    Image(systemName: "star.fill")
-                        .foregroundColor(.yellow)
-                        .font(.caption)
+                HStack {
+                    Text(store.name)
+                        .font(.title)
+                        .fontWeight(.bold)
+                    Spacer()
+                    Image(store.logoImage)
                 }
-            }.padding()
-            
-            Text("Produtos")
-                .font(.title2)
-                .fontWeight(.bold)
-                .padding()
-            
-            Spacer()
+                .padding(.vertical, 8)
+                .padding(.horizontal)
+                
+                HStack {
+                    Text(store.location)
+                    
+                    Spacer()
+                    
+                    ForEach(1...store.stars, id: \.self) { _ in
+                        Image(systemName: "star.fill")
+                            .foregroundColor(.yellow)
+                            .font(.caption)
+                    }
+                }.padding()
+                
+                Text("Produtos")
+                    .font(.title2)
+                    .fontWeight(.bold)
+                    .padding()
+                
+                ForEach(store.products) { product in
+                    HStack(spacing: 8) {
+                        VStack(alignment: .leading, spacing: 8) {
+                            Text(product.name)
+                                .bold()
+                            
+                            Text(product.description)
+                                .foregroundColor(.black.opacity(0.5))
+                            
+                            Text("\(product.price)")
+                        }
+                        
+                        Spacer()
+                        
+                        Image(product.image)
+                            .resizable()
+                            .scaledToFit()
+                            .cornerRadius(12)
+                            .frame(width: 120, height: 120)
+                            .shadow(color: .black.opacity(0.3), radius: 12, x: 6, y: 8)
+                    }
+                    .padding()
+                }
+            }
         }
         .navigationTitle(store.name)
         .navigationBarTitleDisplayMode(.inline)
